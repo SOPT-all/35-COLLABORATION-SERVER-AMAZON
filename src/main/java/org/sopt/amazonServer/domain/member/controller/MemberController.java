0afd8e3b2d560696734f52ff7251b3dbbecc74ce
ckkim817 <ckkim817@konkuk.ac.kr>
2024-11-19 15:10:51 +0900

    feat: 유저 검색 기록 조회 엔드포인트 작성 (#5)

---
+++ src/main/java/org/sopt/amazonServer/domain/member/controller/MemberController.java
@@ -0,0 +1,28 @@
+package org.sopt.amazonServer.domain.member.controller;
+
+import java.util.List;
+import org.sopt.amazonServer.domain.member.model.dto.SearchHistoryResponse;
+import org.sopt.amazonServer.domain.member.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/search-history")
+    public ResponseEntity<List<SearchHistoryResponse>> getSearchHistoryList(
+            @RequestHeader final Long memberId
+    ) {
+        return ResponseEntity.ok(memberService.fetchSearchHistoryList(memberId));
+    }
+}
