f34eefd6e7c54364b5dcaeba9c869a4a95c6f9c4
gahyuun <pgh123166@naver.com>
2024-11-19 16:47:04 +0900

    fix: 인기순 정렬 수정 (#6)

--- src/main/java/org/sopt/amazonServer/domain/product/service/ProductService.java
+++ src/main/java/org/sopt/amazonServer/domain/product/service/ProductService.java
@@ -1,34 +1,48 @@
 package org.sopt.amazonServer.domain.product.service;
 
-import org.sopt.amazonServer.domain.cart.repostiory.CartRepository;
-import org.sopt.amazonServer.domain.product.model.dto.GetProductRequest;
-import org.sopt.amazonServer.domain.product.model.entity.ProductEntity;
-import org.sopt.amazonServer.domain.product.model.enums.Sort;
-import org.sopt.amazonServer.domain.product.repository.ProductRepository;
-import org.springframework.stereotype.Component;
-
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.sopt.amazonServer.domain.cart.repostiory.CartRepository;
+import org.sopt.amazonServer.domain.member.repository.MemberRepository;
+import org.sopt.amazonServer.domain.product.model.dto.GetProductResponse;
+import org.sopt.amazonServer.domain.product.model.entity.ProductEntity;
+import org.sopt.amazonServer.domain.product.model.enums.Sort;
+import org.sopt.amazonServer.domain.product.repository.ProductRepository;
+import org.sopt.amazonServer.global.exception.BusinessException;
+import org.sopt.amazonServer.global.exception.ErrorType;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ProductService {
     private final ProductRepository productRepository;
     private final CartRepository cartRepository;
+    private final MemberRepository memberRepository;
 
-    public ProductService(ProductRepository productRepository,CartRepository cartRepository) {
+    public ProductService(ProductRepository productRepository, CartRepository cartRepository,
+                          MemberRepository memberRepository) {
         this.productRepository = productRepository;
         this.cartRepository = cartRepository;
+        this.memberRepository = memberRepository;
     }
 
-    public List<GetProductRequest> fetchProducts(String keyword, Sort sort, Long memberId) {
-        List<ProductEntity> productList = productRepository.findByNameContaining(keyword);
-        sortProducts(productList,sort);
-       return productList.stream()
-                .map(product -> {
-                    return new GetProductRequest(
+    public List<GetProductResponse> fetchProducts(String keyword, Sort sort, Long memberId) {
+        if (!memberRepository.existsById(memberId)) {
+            throw new BusinessException(ErrorType.NOT_FOUND_MEMBER);
+        }
+        List<ProductEntity> productList;
+        if (keyword == null) { // 키워드가 없을 땐 전체 데이터 조회
+            productList = productRepository.findAll();
+        } else {
+            productList = productRepository.findByNameContaining(keyword);
+        }
+        // 상품 정렬
+        sortProducts(productList, sort);
+
+        return productList.stream()
+                .map(product -> new GetProductResponse(
                         product.getId(),
                         product.getImage(),
                         product.getBrand(),
@@ -40,22 +54,24 @@ public class ProductService {
                         product.getDiscountRate(),
                         product.isFreeDelivery(),
                         product.getDeliveryDate().toLocalDateTime()
-                        .toLocalDate()
-                        .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                                .toLocalDate()
+                                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), // TODO: DateFormatter 분리
                         product.getFreeDeliveryStandard(),
-                            cartRepository.existsByMemberIdAndProductId(memberId,product.getId()) // 장바구니 여부
-                );})
+                        cartRepository.existsByMemberIdAndProductId(memberId, product.getId())
+                ))
                 .toList();
     }
 
 
-    public final void sortProducts(List<ProductEntity> productList, Sort sort) {
+    private void sortProducts(List<ProductEntity> productList, Sort sort) {
         Map<Sort, Comparator<ProductEntity>> comparatorMap = new HashMap<>();
 
-        comparatorMap.put(Sort.REVIEWCOUNT, Comparator.comparing(ProductEntity::getReviewCount).reversed());
-        comparatorMap.put(Sort.LOWPRICE, Comparator.comparing(ProductEntity::getPrice));
-        comparatorMap.put(Sort.LATESTPRODUCTS, Comparator.comparing(ProductEntity::getLaunchDate).reversed());
-        comparatorMap.put(Sort.POPULARITY,Comparator.comparing(ProductEntity::getSales).reversed());
+        comparatorMap.put(Sort.REVIEW_COUNT, Comparator.comparing(ProductEntity::getReviewCount).reversed());
+        comparatorMap.put(Sort.LOW_PRICE, Comparator.comparing(ProductEntity::getPrice));
+        comparatorMap.put(Sort.LATEST_PRODUCTS, Comparator.comparing(ProductEntity::getLaunchDate).reversed());
+        comparatorMap.put(Sort.POPULARITY,
+                Comparator.comparing(product -> (product.getSales() + product.getReviewCount()) * product.getRating()));
+        comparatorMap.put(Sort.SALES, Comparator.comparing(ProductEntity::getSales).reversed());
 
         Comparator<ProductEntity> comparator = comparatorMap.get(
                 sort
