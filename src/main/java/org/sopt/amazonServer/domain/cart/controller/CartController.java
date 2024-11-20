78af16699153ec88e293b891bf25f2cea86c5920
gahyuun <pgh123166@naver.com>
2024-11-21 00:32:59 +0900

    feat: Cart Controller 생성 (#9)

---
+++ src/main/java/org/sopt/amazonServer/domain/cart/controller/CartController.java
@@ -0,0 +1,34 @@
+package org.sopt.amazonServer.domain.cart.controller;
+
+import jakarta.validation.constraints.Positive;
+import org.sopt.amazonServer.domain.cart.model.dto.CartResponse;
+import org.sopt.amazonServer.domain.cart.service.CartService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequestMapping("/api/v1/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @DeleteMapping("/{productId}")
+    ResponseEntity<CartResponse> deleteProductInCart(
+            @Positive(message = "productId는 양수여야 합니다.")
+            @PathVariable(name = "productId") final Long productId, // TODO: productId 없을 때 500 에러 막기
+            @RequestHeader("memberId") final Long memberId
+    ) {
+        return ResponseEntity.ok(cartService.removeProduct(productId, memberId));
+    }
+
+}
---
+++ src/main/java/org/sopt/amazonServer/domain/cart/model/dto/CartResponse.java
@@ -0,0 +1,7 @@
+package org.sopt.amazonServer.domain.cart.model.dto;
+
+public record CartResponse(
+        int cartCount
+) {
+
+}
